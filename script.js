4c92fcbed8df11324327aee78760819d0a89e8c6
Max <msizer@uclan.ac.uk>
2020-12-05 10:39:38 +0000

    updated code

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 
 $.getJSON(
-"https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumCasesByPublishDate&metric=cumDeaths28DaysByDeathDate&metric=newCasesByPublishDate&metric=newDeaths28DaysByDeathDate&format=json",
+"https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumCasesByPublishDate&metric=newCasesByPublishDate&metric=newDeaths28DaysByPublishDate&metric=cumDeaths28DaysByPublishDate&format=json",
 function(data) {
   console.log(data);
 
-  var totdeaths = data.body[1].cumDeaths28DaysByDeathDate;
-  var deaths = data.body[1].newDeaths28DaysByDeathDate;
+  var totdeaths = data.body[0].cumDeaths28DaysByPublishDate;
+  var deaths = data.body[0].newDeaths28DaysByPublishDate;
   var totcases = data.body[0].cumCasesByPublishDate;
   var cases = data.body[0].newCasesByPublishDate;
+  var date = data.body[0].date;
 
   $('.totdeaths').append(totdeaths);
   $('.deaths').append(deaths);
   $('.totcases').append(totcases);
   $('.cases').append(cases);
+  $('.date').append(date);
   }
 );
